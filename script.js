2862b266e212513da4b64ca644e54b81d43c0c47
Deependra-Negi <916deepaknegi@gmail.com>
2021-11-25 12:52:45 +0530

    fixed bar height

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
+function loadCanvas() {
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
 
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+    // Draw graph
 
-// Draw graph
+    let width = window.innerWidth;
+    const height = 200;
+    canvas.width = width;
+    canvas.height = height;
 
-let width = window.innerWidth;
-const height = 200;
-canvas.width = width;
-canvas.height = height;
+    // ctx.fillStyle = "#f9f9f9";
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#dbdbdb";
+    let min = Math.ceil(30);
+    let max = Math.floor(100);
 
-ctx.fillStyle = "pink";
-
-let arr = Array.from({ length: width }, () => Math.floor(Math.random() * 100));
-for (let i = 0; i < arr.length; i += 9){
-    arr[i] = -arr[i];
-    i++;
+    let arr = Array.from({ length: width }, () => Math.floor(Math.random() * (max-min)+min));
+    for (let i = 0; i < arr.length; i += 8){
+        arr[i] = -arr[i];
+        i++;
+    }
+    console.log(arr)
+    for (let i = 0; i < arr.length; i++){
+        ctx.fillRect(i, (arr[i]<0 && arr[i]<-30) ? 100-(Math.ceil(Math.random()*30)) : 100, 5, -arr[i])
+        i += 5;
+    }
+    canvas.fillStyle = "green"   
 }
-console.log(arr)
-for (let i = 0; i < arr.length; i++){
-    ctx.fillRect(i, 100, 5, -arr[i])
-    i += 5;
-}
-canvas.fillStyle = "green"
