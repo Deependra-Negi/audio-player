77e04fe39201ad97da34849df6f50c2ac7fed438
Deependra-Negi <916deepaknegi@gmail.com>
2021-11-25 21:36:14 +0530

    audio controls and seeking added

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// window.addEventListener('resize', function(event){
-//     // console.log(window.innerWidth)
-//     loadCanvas();
-// });
+const canvas = document.getElementById("canvas");
+let seek = 0;
+let jump_distance = 0;
+let device_width = window.innerWidth;
 
-document.getElementById("canvas").addEventListener('click', e => console.log(e.x))
-window.addEventListener('click', e => console.log(innerWidth))
+const audio = new Audio('./media/lofi.mp3');
+let duration;
+canvas.addEventListener('click', e=>jump(e));
+window.addEventListener('click', ()=>device_width=innerWidth)
+
+function jump(e) {
+    seek = e.x;
+    jump_distance = (seek * 100) / device_width;
+    audio.currentTime = (duration * jump_distance) / 100;
+}
 
 function loadCanvas() {
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
+    duration = audio.seekable.end(0);
 
     // Draw graph
 
@@ -17,8 +26,6 @@ function loadCanvas() {
     canvas.width = width-32;
     canvas.height = height;
 
-    // ctx.fillStyle = "#f9f9f9";
-    // ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#dbdbdb";
 
     let min = Math.ceil(30);
@@ -26,6 +33,32 @@ function loadCanvas() {
 
     for (let i = 0; i < (width/2)-16; i++){
         ctx.fillRect(i+i, Math.floor(Math.random()*(max-min)+min), 5, 100)
-        i +=2.5;
-    }  
+        i +=3;
+    }
 }
+
+const play_btn = document.getElementById('play');
+const pause_btn = document.getElementById('pause');
+
+function play() {
+    pause_btn.style.display = "block"
+    play_btn.style.display = "none"
+    audio.play();
+}
+function pause() {
+    play_btn.style.display="block"
+    pause_btn.style.display = "none"
+    audio.pause();
+    
+    const audio_current = audio.currentTime;
+    console.log(`Current : ${audio_current}`);
+}
+
+//008D9A
+
+//attribute
+//icons - https://www.flaticon.com/
+//play - By Webalys
+//pause - By Kiranshastry
+//vol - By Dave Gandy
+// fast -  By inkubators
