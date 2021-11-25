254e7cb61eb5b126673942c1c2bf7a2c9e123777
Deependra-Negi <916deepaknegi@gmail.com>
2021-11-25 22:57:58 +0530

    volume and fastforward control added

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const canvas = document.getElementById("canvas");
-let seek = 0;
-let jump_distance = 0;
 let device_width = window.innerWidth;
 
-const audio = new Audio('./media/lofi.mp3');
+const audio = new Audio('./media/audio.mp3');
 let duration;
 canvas.addEventListener('click', e=>jump(e));
 window.addEventListener('click', ()=>device_width=innerWidth)
 
 function jump(e) {
-    seek = e.x;
-    jump_distance = (seek * 100) / device_width;
+    let seek = e.x;
+    let jump_distance = (seek * 100) / device_width;
     audio.currentTime = (duration * jump_distance) / 100;
 }
 
@@ -20,38 +18,43 @@ function loadCanvas() {
     duration = audio.seekable.end(0);
 
     // Draw graph
-
     let width = window.innerWidth;
     const height = 200;
     canvas.width = width-32;
     canvas.height = height;
-
     ctx.fillStyle = "#dbdbdb";
 
-    let min = Math.ceil(30);
-    let max = Math.floor(100);
-
     for (let i = 0; i < (width/2)-16; i++){
-        ctx.fillRect(i+i, Math.floor(Math.random()*(max-min)+min), 5, 100)
+        ctx.fillRect(i+i, Math.floor(Math.random()*(100-30)+30), 5, 100)
         i +=3;
     }
 }
 
+// Buttons functionality
 const play_btn = document.getElementById('play');
 const pause_btn = document.getElementById('pause');
+const vol_btn = document.getElementById('volume');
 
 function play() {
     pause_btn.style.display = "block"
     play_btn.style.display = "none"
     audio.play();
 }
+
 function pause() {
     play_btn.style.display="block"
     pause_btn.style.display = "none"
     audio.pause();
-    
-    const audio_current = audio.currentTime;
-    console.log(`Current : ${audio_current}`);
+}
+
+let volume_state = true;
+function handleVolume() {
+    volume_state = !volume_state;
+    volume_state ? (vol_btn.style.backgroundImage = 'url(./media/icons/volume.png)') && (audio.muted = false) : (vol_btn.style.backgroundImage = 'url(./media/icons/mute.png)') && (audio.muted = true);
+}
+
+function fastForward() {
+    audio.currentTime = audio.currentTime + 10;
 }
 
 //008D9A
@@ -62,3 +65,4 @@ function pause() {
 //pause - By Kiranshastry
 //vol - By Dave Gandy
 // fast -  By inkubators
+//mute - freepik
