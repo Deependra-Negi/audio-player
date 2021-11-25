5b7a39e47e38b44050816131eae50f54d105bd0c
Deependra-Negi <916deepaknegi@gmail.com>
2021-11-25 17:03:39 +0530

    UI changed, buttons added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// window.addEventListener('resize', function(event){
+//     // console.log(window.innerWidth)
+//     loadCanvas();
+// });
+
+document.getElementById("canvas").addEventListener('click', e => console.log(e.x))
+window.addEventListener('click', e => console.log(innerWidth))
+
 function loadCanvas() {
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
@@ -6,24 +14,18 @@ function loadCanvas() {
 
     let width = window.innerWidth;
     const height = 200;
-    canvas.width = width;
+    canvas.width = width-32;
     canvas.height = height;
 
     // ctx.fillStyle = "#f9f9f9";
     // ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#dbdbdb";
+
     let min = Math.ceil(30);
     let max = Math.floor(100);
 
-    let arr = Array.from({ length: width }, () => Math.floor(Math.random() * (max-min)+min));
-    for (let i = 0; i < arr.length; i += 8){
-        arr[i] = -arr[i];
-        i++;
-    }
-    console.log(arr)
-    for (let i = 0; i < arr.length; i++){
-        ctx.fillRect(i, (arr[i]<0 && arr[i]<-30) ? 100-(Math.ceil(Math.random()*30)) : 100, 5, -arr[i])
-        i += 5;
-    }
-    canvas.fillStyle = "green"   
+    for (let i = 0; i < (width/2)-16; i++){
+        ctx.fillRect(i+i, Math.floor(Math.random()*(max-min)+min), 5, 100)
+        i +=2.5;
+    }  
 }
